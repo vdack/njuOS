5a7aa3ad767ce6b807f989e0c792aeb2cec54763
vdack <thousandawn@126.com>
2024-05-02 20:01:15 +0800

    base

--- abstract-machine/scripts/isa/x86.mk
+++ abstract-machine/scripts/isa/x86.mk
@@ -3,3 +3,4 @@ CFLAGS  += -m32 -fno-pic -fno-omit-frame-pointer -march=i386
 CFLAGS  += -fcf-protection=none # remove endbr32 in Ubuntu 20.04 with a CPU newer than Comet Lake
 ASFLAGS += -m32 -fno-pic
 LDFLAGS += -melf_i386
+CFLAGS += --param=min-pagesize=0
