fd6d5dc3d57b61f1869b977b16d2ff9ded6f4452
tracer-nju <tracer@nju.edu.cn>
2024-05-08 15:14:31 +0800

    Linux vdackPC 6.5.0-0.deb12.4-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.5.10-1~bpo12+1 (2023-11-23) x86_64 GNU/Linux  15:14:31 up 3 days, 42 min,  3 users,  load average: 0.87, 0.79, 0.66

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -1,7 +1,42 @@
 #include <common.h>
 
+#ifndef _MY_LOCK
+#define _MY_LOCK
+
+#include "am.h"
+#define MY_LOCKED 1
+#define MY_UNLOCKED 0
+
+typedef struct _lock_type {
+    int flag;
+} lock_t;
+
+void lock_init(lock_t* lock) {
+    lock->flag = 0;
+} 
+
+void lock_acquire(lock_t* lock) {
+    while(atomic_xchg(&lock->flag, MY_LOCKED) == MY_LOCKED);
+}
+
+void lock_release(lock_t* lock) {
+    int rc = atomic_xchg(&lock->flag, MY_UNLOCKED);
+    panic_on(rc == MY_LOCKED, "release before acquire!!!");
+}
+
+bool try_lock_acquire(lock_t* lock) {
+    int rc = atomic_xchg(&lock->flag, MY_LOCKED);
+    return rc == MY_UNLOCKED;
+}
+
+
+#endif
+
 typedef struct _header {
-    
+    lock_t mutex;
+    size_t size;
+    uintptr_t next;
+    bool occupied;
 } header;
 
 static void *kalloc(size_t size) {
@@ -26,6 +61,7 @@ static void pmm_init() {
         "Got %d MiB heap: [%p, %p)\n",
         pmsize >> 20, heap.start, heap.end
     );
+    printf("size of header: %d\n",sizeof(header));
 }
 
 MODULE_DEF(pmm) = {
