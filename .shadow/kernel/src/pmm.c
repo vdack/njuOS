cdfe0c3f66ebef7b726a7e89bcc38f0f1cef60ce
tracer-nju <tracer@nju.edu.cn>
2024-05-09 22:56:53 +0800

    Linux vdackPC 6.5.0-0.deb12.4-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.5.10-1~bpo12+1 (2023-11-23) x86_64 GNU/Linux  22:56:53 up 4 days,  8:25,  4 users,  load average: 0.95, 0.81, 0.80

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -246,6 +246,7 @@ static void pmm_init() {
         write_header(last_addr, small_header);
         left_size -= SMALL_SIZE;
     }
+    printf("after small \n");
     header_t small_end_header = construct_header(SMALL_SIZE - HEADER_SIZE, last_addr);
     write_header((first_buddy_addr - SMALL_SIZE), small_end_header);
     first_small_addr = (header_t*)last_addr;
