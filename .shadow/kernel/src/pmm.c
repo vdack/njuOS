3838f453dc3cde02e28fbe2299fe22cd144673e5
tracer-nju <tracer@nju.edu.cn>
2024-05-10 20:44:45 +0800

    Linux vdackPC 6.5.0-0.deb12.4-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.5.10-1~bpo12+1 (2023-11-23) x86_64 GNU/Linux  20:44:45 up 5 days,  6:12,  4 users,  load average: 1.01, 0.83, 0.89

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -200,10 +200,10 @@ static void kfree(void *ptr) {
     } else {
         // free buddy. 
         header_t* h_addr = (header_t*)((intptr_t)ptr - HEADER_SIZE);
-        // lock_acquire(&h_addr->mutex);
-        // h_addr->occupied = false;
-        // lock_release(&h_addr->mutex);
-        buddy_free(h_addr);
+        lock_acquire(&h_addr->mutex);
+        h_addr->occupied = false;
+        lock_release(&h_addr->mutex);
+        // buddy_free(h_addr);
     }
 
 }
