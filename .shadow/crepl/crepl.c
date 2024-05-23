c22f32da4b8ad8ee7f3b07c62027010c0aa524c4
tracer-nju <tracer@nju.edu.cn>
2024-05-23 19:34:26 +0800

    Linux vdackPC 6.5.0-0.deb12.4-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.5.10-1~bpo12+1 (2023-11-23) x86_64 GNU/Linux  19:34:26 up  2:03,  3 users,  load average: 0.77, 0.74, 0.78

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -19,8 +19,9 @@ static char* buffer_args[10];
 static char* target_args[9];
 
 static inline void init_env(char* buffer_name, char* target_name) {
-    setenv("LD_LIBRARY_PATH", "/tmp/", 1);
-    system("echo $LD_LIBRARY_PATH");
+    // setenv("LD_LIBRARY_PATH", "/tmp/", 1);
+    // system("echo $LD_LIBRARY_PATH");
+    system("export LD_LIBRARY_PATH=/tmp");
 
     buffer_args[0] = strdup("gcc");
     buffer_args[1] = strdup("-fPIC");
