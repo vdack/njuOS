db77a01bcd35b7ff7f7dfd38bb21201276f637cf
tracer-nju <tracer@nju.edu.cn>
2024-07-19 20:52:08 +0800

    Linux vdackPC 6.5.0-0.deb12.4-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.5.10-1~bpo12+1 (2023-11-23) x86_64 GNU/Linux  20:52:08 up 1 day,  9:59,  3 users,  load average: 0.26, 0.20, 0.34

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -34,10 +34,11 @@ static void os_init() {
 
 static void os_run() {
     
+    // for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
+        //     putch(*s == '*' ? '0' + cpu_current() : *s);
+        // }
     while (1) {
-        for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
-            putch(*s == '*' ? '0' + cpu_current() : *s);
-        }
+        
         yield();
     } ;
 }
